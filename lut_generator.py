51c0de7c2b020b9b8e4eaaef41f9b550a8830f21
zacrogers <38870655+zacrogers@users.noreply.github.com>
2019-10-12 15:10:39 +1300

    Update lut_generator.py

--- lut_generator.py
+++ lut_generator.py
@@ -7,12 +7,15 @@ def generateSine(length, signed):
     is_signed = ("unsigned", "signed")[signed]
     filename  = "sine{}_{}".format(length, is_signed)
     
+    # Filename
     print(filename + ".h")
+    
+    # Text to write to file
     print("#ifndef {}".format(filename.upper()))
-    print("#def {}".format(filename.upper()))
+    print("#def {}\n".format(filename.upper()))
     print("float {}[{}] = {{".format(filename, length))
     
-    for i in range (length):
+    for i in range(length):
         sample = (np.sin((2*np.pi*i)/length))
         if signed:
             sample = (sample+1)/2
@@ -23,6 +26,10 @@ def generateSine(length, signed):
     print("#endif")
     
     
+def mapVals(x, in_min, in_max, out_min, out_max):
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+
+
 def getInput():
     length = input("Enter number of samples(default:1024):")
     signed = input("Signed?(y/n):")
